ea5804ee03ed7d944170227267e4ff4a03447084
David Renz <davidrenz60@gmail.com>
2023-12-13 17:49:53 -0800

    more fix

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'davidrenz60@gmail.com'
+  config.mailer_sender = 'noreply@football-picks-hub.fly.dev'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
