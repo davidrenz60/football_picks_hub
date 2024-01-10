9aeff40ad201ee1e8ad14b6f5c4043e8997a4afe
David Renz <davidrenz60@gmail.com>
2024-01-10 12:47:10 -0800

    remove admin code

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,4 @@
 class DashboardController < ApplicationController
   def index
-    admin_user = User.find_by(email: 'davidrenz60@gmail.com')
-    admin_user.admin = true
-    admin_user.save
   end
 end
